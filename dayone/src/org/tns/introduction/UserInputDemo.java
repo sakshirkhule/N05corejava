646a474a3739887679dbe9d8ce351017674558e4
SAKSHI <sakshikhule3010@gmail.com>
2023-07-20 13:07:37 +0630

    first commit

---
+++ dayone/src/org/tns/introduction/UserInputDemo.java
@@ -0,0 +1,33 @@
+// program to demonstrate on userinput
+package org.tns.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		/*System.out.println("Enter the value of num");
+		int num=sc.nextInt();
+		System.out.println("The value of num is "+num);
+		
+		System.out.println("Enter the value of flaot");
+		Float num1=sc.nextFloat();
+		System.out.println("The value of num1 is "+num1);
+		
+		System.out.println("Enter the value of char");
+		char num2=sc.next().charAt(0);
+		System.out.println("The value of Charchrais "+num2);*/
+		
+		System.out.println("Enter the value of char");
+		String num3=sc.nextLine();
+		//sc.next();
+		System.out.println("The value of Charchrais "+num3);
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
