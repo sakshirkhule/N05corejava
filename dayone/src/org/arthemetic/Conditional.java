ae16626de5a8331e5a4b3d4d7be377992c590f02
SAKSHI <sakshikhule3010@gmail.com>
2023-07-21 13:15:02 +0630

    first commit

---
+++ dayone/src/org/arthemetic/Conditional.java
@@ -0,0 +1,20 @@
+package org.arthemetic;
+import java.util.Scanner;
+public class Conditional {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int age=sc.nextInt();
+		int Weight=sc.nextInt();
+		
+		if((age>18)&&(Weight>50)) {
+			System.out.print("Eligible for donation");
+		}
+		else {
+			System.out.print("not eligible for donation");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
